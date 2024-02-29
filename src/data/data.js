48d1f47d40c271dfd1e887c9850a58e8c98bd392
Miguel Contreras <macc07112005c80@gmail.com>
2024-02-28 22:53:37 -0500

    Add Feature: Home Page

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    projectName: "Movie - Seat",
+    imageUrl: "/p1.webp",
+    description:
+      "Un sistema interactivo de gestión de asientos para un cine o auditorio. Permite a los usuarios seleccionar y deseleccionar asientos, mostrando información en tiempo real sobre la cantidad de asientos seleccionados y el precio total.",
+    tecnologies: ["astro", "javascript", "html", "css"],
+    demo: "/movie-seat",
+  },
+];
