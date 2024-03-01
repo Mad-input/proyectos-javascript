caf0a8382151a0c79553477d2e544ad6266f4d06
Miguel Contreras <macc07112005c80@gmail.com>
2024-02-29 21:36:06 -0500

    Add styles: Home Page

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,12 @@ export const projects = [
     imageUrl: "/p1.webp",
     description:
       "Un sistema interactivo de gestión de asientos para un cine o auditorio. Permite a los usuarios seleccionar y deseleccionar asientos, mostrando información en tiempo real sobre la cantidad de asientos seleccionados y el precio total.",
-    tecnologies: ["astro", "javascript", "html", "css"],
+    tecnologies: [
+      { name: "Astro", icon: "/astro.svg" },
+      { name: "HTML", icon: "/html.svg" },
+      { name: "CSS", icon: "/css.svg" },
+      { name: "Javascript", icon: "/javascript.svg" },
+    ],
     demo: "/movie-seat",
   },
 ];
